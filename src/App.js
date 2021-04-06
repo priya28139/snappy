4a1dbecce3bceb560d5aeb01df398465c12b7a81
Priya Aswani <priya.aswani28139@gmail.com>
2021-04-06 13:23:40 +0400

    Render out the UploadForm component in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import UploadForm from "./components/UploadForm";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <UploadForm />
     </div>
   );
 };
